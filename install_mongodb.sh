d02176f8f376beb5e808024f9dc8d3099827176d
Osipov Stanislav <st.osipov@yandex.ru>
2020-06-21 12:28:44 +0300

    homework: typo fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,7 +4,7 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
 # Add mongo repo to sources list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb- org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 # Update packets database
 apt-get update
